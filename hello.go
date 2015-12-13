0bd00316f21dcdd500b2b73892fdbea945accbdc
Ben Siggers <bsiggers@salesforce.com>
2015-12-13 12:38:53 -0800

    Setup structure - working

--- hello.go
+++ hello.go
@@ -2,10 +2,28 @@ package main
 
 import (
         "fmt"
-        "log"
+        "io/ioutil"
+        "path/filepath"
         "gopkg.in/yaml.v2"
 )
 
+type Sudoku struct {
+  Board [][]int
+}
+
 func main() {
+  filename, _ := filepath.Abs("./sudoku.yaml")
+  yamlFile, err := ioutil.ReadFile(filename)
+  if err != nil {
+    panic(err)
+  }
+  var sudoku Sudoku
+
+  err = yaml.Unmarshal(yamlFile, &sudoku)
+  if err != nil {
+      panic(err)
+  }
+
   fmt.Printf("Hello, test world\n")
+  fmt.Printf("%#v\n", sudoku.Board)
 }
